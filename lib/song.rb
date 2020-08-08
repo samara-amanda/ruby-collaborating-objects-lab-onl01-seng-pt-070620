412e8f4f122f46f43cf5eb109a8e99aadb888e0f
Samara Grant <samara.amanda01@gmail.com>
2020-08-08 04:18:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,8 +12,8 @@ class Song
   end
 
   def self.new_by_filename(name)
-    artist_name = file.split(" - ")[0]
-    song_name = file.split(" - ")[1]
+    artist_name = name.split(" - ")[0]
+    song_name = name.split(" - ")[1]
     song = Song.new(song_name)
     song.artist_name = artist_name
     song
